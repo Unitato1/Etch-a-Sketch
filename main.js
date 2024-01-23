c80d2aa2b98cc88dc551861a6577304156280051
Unitato1 <robertsvihla@gmail.com.github.com>
2024-01-23 13:32:55 +0100

    added styles and also board

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("#3");
+const container = document.querySelector("#container");
+
+function createField() {
+  const field = document.createElement("div");
+  field.className = "field";
+  return field;
+}
+
+function createRow() {
+  const row = document.createElement("div");
+  row.className = "row";
+  return row;
+}
+
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    let row = createRow();
+    for (let j = 0; j < gridSize; j++) {
+      row.appendChild(createField());
+    }
+    container.appendChild(row);
+  }
+}
+
+createGrid(16);
