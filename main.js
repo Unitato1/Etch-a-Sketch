b0073c23637d832ed898f54d5a28418c5795cf94
Unitato1 <robertsvihla@gmail.com.github.com>
2024-01-23 19:47:26 +0100

    added more buttons and new click to draw fnc

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-console.log("#3");
 const container = document.querySelector("#container");
+const buttons = document.querySelectorAll("li > button");
+const rainbow = document.querySelector("#rainbow");
+const colorElement = document.querySelector("#color");
+const size = document.querySelector("#size");
+
+let color = rgb();
+let mousedown = false;
+let mode = null;
+let gridSize = 16;
+let border = "field";
+let fields = [];
+
+rainbow.style.background = color;
+colorElement.style.background = color;
 
 function createField() {
   const field = document.createElement("div");
-  field.className = "field";
+  field.className = border;
+  field.addEventListener("mouseup", () => {
+    mousedown = false;
+  });
+  field.addEventListener("mousedown", (event) => {
+    mousedown = !mousedown;
+    draw(event);
+  });
+  field.addEventListener("mouseover", draw);
   return field;
 }
 
@@ -14,13 +35,67 @@ function createRow() {
 }
 
 function createGrid(gridSize) {
+  fields = [];
   for (let i = 0; i < gridSize; i++) {
     let row = createRow();
     for (let j = 0; j < gridSize; j++) {
-      row.appendChild(createField());
+      let field = createField();
+      row.appendChild(field);
+      fields.push(field);
     }
     container.appendChild(row);
   }
 }
 
+function draw(event) {
+  if (mousedown) {
+    const field = event.target;
+    if (mode === "rainbow") {
+      color = rgb();
+      rainbow.style.background = color;
+    }
+    field.style.background = color;
+  }
+}
+function rgb() {
+  let createNum = () => {
+    return Math.round(Math.random() * 255);
+  };
+  return `rgba(${createNum()},${createNum()},${createNum()})`;
+}
+function chooseMode(event) {
+  const button = event.target;
+  mode = button.textContent.toLowerCase();
+  if (mode === "black") {
+    color = "black";
+  } else if (mode === "eraser") {
+    color = "white";
+  } else if (mode === "color") {
+    color = rgb();
+    colorElement.style.background = color;
+  } else if (mode === "new grid") {
+    gridSize = size.value > 64 ? 64 : size.value;
+    container.innerHTML = "";
+    createGrid(gridSize);
+  } else if (mode === "clear") {
+    clear();
+  } else if (mode === "border") {
+    border = border === "field" ? "field-no-border" : "field";
+    changeBorder(border);
+  }
+}
+function clear() {
+  fields.forEach((field) => {
+    field.style.background = "white";
+  });
+}
+function changeBorder(className) {
+  fields.forEach((field) => {
+    field.className = className;
+  });
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", chooseMode);
+});
 createGrid(16);
